a8886af525398de0477dfcf54ceea7955388c260
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-21 18:09:54 +0900

    fix

--- lua/telescope/_extensions/hotprojects.lua
+++ lua/telescope/_extensions/hotprojects.lua
@@ -38,7 +38,7 @@ return require("telescope").register_extension {
             opts = opts or {}
             opts.cwd = opts.cwd or vim.fn.getcwd()
             local home_dir = os.getenv("HOME")
-            local command = {"cat", home_dir .. "/.config/local/hotstation"}
+            local command = {"cat ", home_dir .. "/.config/local/hotstation"}
 
             local seen = {};
             local string_entry_maker = make_entry.gen_from_string()
