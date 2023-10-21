c65109fbe9ddf791ac4fda45339eaf7b2a89a260
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-21 14:50:24 +0900

    custom starts

--- lua/telescope/_extensions/hotprojects.lua
+++ lua/telescope/_extensions/hotprojects.lua
@@ -25,7 +25,7 @@ return require("telescope").register_extension {
       opts = opts or {}
       opts.cwd = opts.cwd or vim.fn.getcwd()
 
-      local command = {"ls", "-1"}
+      local command = {"cat", "$HOME/.config/local/hotstation"}
 
       local seen = {};
       local string_entry_maker = make_entry.gen_from_string()
