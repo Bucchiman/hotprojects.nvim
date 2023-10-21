206ce51e41d7bb5d010668754ea7882e9387445d
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-10-21 17:18:47 +0900

    fix for another machines

--- lua/telescope/_extensions/hotprojects.lua
+++ lua/telescope/_extensions/hotprojects.lua
@@ -20,52 +20,65 @@ local make_entry = require("telescope.make_entry")
 local api = vim.api
 local home_dir = vim.env.HOME
 
+
+vim.api.nvim_set_keymap("n", "<C-s><C-a>", function ()
+    local f = io.open(home_dir .. "/.config/local/hotstation", "r")
+    local cwd = vim.fn.getcwd()
+    -- if f == nill then
+    --     print("failed")
+    -- else
+    f:write(cwd)
+    f:close()
+    -- end
+end, {noremap = true, silent = true})
+
+
 return require("telescope").register_extension {
-  exports = {
-    hotproject = function(opts)
-      opts = opts or {}
-      opts.cwd = opts.cwd or vim.fn.getcwd()
+    exports = {
+        show_hotproject = function(opts)
+            opts = opts or {}
+            opts.cwd = opts.cwd or vim.fn.getcwd()
 
-      local command = {"cat", home_dir .. "/.config/local/hotstation"}
+            local command = {"cat", home_dir .. "/.config/local/hotstation"}
 
-      local seen = {};
-      local string_entry_maker = make_entry.gen_from_string()
-      opts.entry_maker = function(string)
-        if (not seen[string]) then
-          seen[string] = true
-          return string_entry_maker(string)
-        else
-          return nil
-        end
-      end
-
-      pickers.new(opts, {
-        prompt_title = 'Hot Project> ',
-        finder = finders.new_oneshot_job(command, opts),
-        sorter = sorters.get_generic_fuzzy_sorter(),
-        attach_mappings = function(prompt_bufnr, map)
-          local insert_hotproject = function()
-            local picker = action_state.get_current_picker(prompt_bufnr)
-            local selections = picker:get_multi_selection()
-            if next(selections) == nil then
-              selections = {picker:get_selection()}
+            local seen = {};
+            local string_entry_maker = make_entry.gen_from_string()
+            opts.entry_maker = function(string)
+                if (not seen[string]) then
+                    seen[string] = true
+                    return string_entry_maker(string)
+                else
+                    return nil
+                end
             end
-            actions.close(prompt_bufnr)
 
-            local hotprojects = {}
-            for _, c in ipairs(selections) do
-              table.insert(hotprojects, c[1])
-            end
-            -- api.nvim_put(hotprojects, "l", true, false)
-            vim.cmd("e " .. hotprojects[1])
-          end
+            pickers.new(opts, {
+                prompt_title = 'Hot Project> ',
+                finder = finders.new_oneshot_job(command, opts),
+                sorter = sorters.get_generic_fuzzy_sorter(),
+                attach_mappings = function(prompt_bufnr, map)
+                    local list_hotproject = function()
+                    local picker = action_state.get_current_picker(prompt_bufnr)
+                    local selections = picker:get_multi_selection()
+                    if next(selections) == nil then
+                        selections = {picker:get_selection()}
+                    end
+                    actions.close(prompt_bufnr)
 
-          map('i', '<CR>', insert_hotproject)
-          map('n', '<CR>', insert_hotproject)
+                    local hotprojects = {}
+                    for _, c in ipairs(selections) do
+                        table.insert(hotprojects, c[1])
+                    end
+                    -- api.nvim_put(hotprojects, "l", true, false)
+                    vim.cmd("e " .. hotprojects[1])
+                end
 
-          return true
-        end,
-      }):find()
-    end
+                map('i', '<CR>', list_hotproject)
+                map('n', '<CR>', list_hotproject)
+
+                return true
+                end,
+            }):find()
+    end,
   }
 }
