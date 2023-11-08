e2528659f2ef29cee2a2b8519762171da4f4030a
Yuki Iwabuchi <8ucchiman@gmail.com>
2023-11-08 11:12:26 +0900

    fix spell

--- lua/telescope/_extensions/hotprojects.lua
+++ lua/telescope/_extensions/hotprojects.lua
@@ -26,7 +26,7 @@ return require("telescope").register_extension {
             opts = opts or {}
             opts.cwd = opts.cwd or vim.fn.getcwd()
             local home_dir = os.getenv("HOME")
-            local hotstation_paty = home_dir .. "/.config/loca/hotstation"
+            local hotstation_paty = home_dir .. "/.config/local/hotstation"
             local command = {"cat", hotstation_paty}
             -- local command = {"cat", "/Users/8ucchiman/.config/local/hotstation"}
 
